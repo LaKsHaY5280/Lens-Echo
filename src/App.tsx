eef6ac1a3ff1ea344768e43983e3ce13ea5e8bc5
Lakshay goyal <akshaybuster5280@gmail.com>
2023-11-09 12:26:21 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./globals.css";
+
+function App() {
+  return (
+    <div className=" ">
+      hello <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
