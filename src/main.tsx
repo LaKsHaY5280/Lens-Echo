02acd727bf7386004c02dc97dc70a163cd813e15
Lakshay goyal <akshaybuster5280@gmail.com>
2023-11-10 18:47:59 +0530

    topbar done

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.tsx'
-import AuthProvider from "./context/authContext.tsx";
+import AuthProvider from "./context/AuthContext.tsx";
 import { QueryProvider } from "./lib/react-query/QueryProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
