8fdc2b2c5166cf9069d3cec321a9abf6b8a753e5
Lakshay goyal <akshaybuster5280@gmail.com>
2023-11-11 20:34:50 +0530

    error fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+// vite.config.js
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
-    
   },
+  // server: {
+  //   historyApiFallback: true,
+  // },
 });
